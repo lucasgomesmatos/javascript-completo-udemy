7ffed9364d0b99bef7ebea8472280029ab1f7c04
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2021-12-09 21:16:29 -0300

    escopo lexico

---
+++ funções/escopo-lexico/index.js
@@ -0,0 +1,16 @@
+const nome = 'Lucas';
+
+function falaNome() {
+  // const nome = 'Maria';
+  console.log(nome);
+}
+
+falaNome();
+
+
+function usaFalanome() {
+  const nome = 'Maria';
+  falaNome()
+}
+
+usaFalanome();
