8552f6802e421e9a23ab485b124702cfb6b89cc5
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2021-12-18 18:42:13 -0300

    funcao geradora

--- funções/funcao-geradora/index.js
+++ funções/funcao-geradora/index.js
@@ -27,3 +27,39 @@ const cont = contador();
 console.log(cont.next().value);
 console.log(cont.next().value);
 console.log(cont.next().value);
+
+
+function* geradora3() {
+  yield 0;
+  yield 1;
+  yield 2;
+}
+
+function* geradora4() {
+  yield* geradora3();
+  yield 3;
+  yield 4;
+  yield 5;
+}
+
+const g4 = geradora4();
+for(let valor of g4) {
+  console.log(valor)
+}
+
+
+function* geradora5() {
+  yield function() {
+    console.log('Vim do y1')
+  }
+  yield function() {
+    console.log('Vim do y2')
+  }
+}
+
+const g5 = geradora5();
+const func1 = g5.next().value;
+const func2 = g5.next().value;
+
+func1();
+func2();
