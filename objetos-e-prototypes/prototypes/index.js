f4ff6b012cd0e1c1a31e88e56eb0933d713dc7c9
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-01-04 08:39:30 -0300

    prototype em js

--- objetos-e-prototypes/prototypes/index.js
+++ objetos-e-prototypes/prototypes/index.js
@@ -2,10 +2,14 @@
 function Pessoa(nome, sobrenome) {
   this.nome = nome;
   this.sobrenome = sobrenome;
-  this.nomeCompleto = () => {
-    return `${nome} ${sobrenome}`;
-  }
 }
+
+Pessoa.prototype.nomeCompleto = function() {
+  return `${this.nome} ${this.sobrenome}`;
+}
+
 const p1 = new Pessoa('Lucas', 'Gomes');
+const data = new Date();
 
 console.log(p1.nomeCompleto())
+console.log(data)
