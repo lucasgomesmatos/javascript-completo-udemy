19ae4fa4366def03b3513a89aee99d177a82eea6
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-01-07 20:55:06 -0300

    object prototype

--- objetos-e-prototypes/manipulando-prototypes/index.js
+++ objetos-e-prototypes/manipulando-prototypes/index.js
@@ -1,5 +1,45 @@
 // new Object -> Object.prototype
 
 const objA = {
-  chave: 'A'
+  chaveA: 'A'
+  // __proto__: Object.prototype
 };
+
+const objB = {
+  chaveB: 'B'
+  // __proto__: objA
+}
+
+Object.setPrototypeOf(objB, objA);
+// console.log(objB.chaveA);
+
+
+function Produto(nome, preco) {
+  this.nome = nome;
+  this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual) {
+  this.preco = this.preco - (this.preco * (percentual / 100));
+}
+
+Produto.prototype.aumento = function(percentual) {
+  this.preco = this.preco + (this.preco * (percentual / 100));
+}
+
+const p1 = new Produto('Camiseta', 50);
+
+
+const p2 = {
+  nome: 'Caneca',
+  preco: 15
+};
+
+Object.setPrototypeOf(p2, Produto.prototype);
+
+p2.aumento(10);
+
+console.log(p1);
+console.log(p2);
+
+const p3 = Object.create(Produto.prototype);
