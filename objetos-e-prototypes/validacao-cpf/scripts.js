96a09ae055eae5eb54bc6b32ab7c4fcecf126d91
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-01-15 22:32:34 -0300

    exercicio validação cpf

---
+++ objetos-e-prototypes/validacao-cpf/scripts.js
@@ -0,0 +1,66 @@
+const form = document.querySelector('.form');
+const cpf = document.querySelector('.search');
+
+cpf.addEventListener('keypress', ()=> {
+  let inputLength = cpf.value.length;
+
+  if(inputLength === 3 || inputLength === 7) {
+    cpf.value += '.';
+  }
+
+  if(inputLength === 11) {
+    cpf.value += '-';
+  }
+
+})
+
+
+function ValidaCPF(cpfEnviado) {
+  Object.defineProperty(this, 'cpfLimpo', {
+    enumerable: true,
+    get: function() {
+      return cpfEnviado.replace(/\D+/g, '');
+    }
+  });
+}
+
+ValidaCPF.prototype.valida = function() {
+  if(typeof this.cpfLimpo === 'undefined') return false;
+  if(this.cpfLimpo.length != 11) return false;
+  if(this.isSequencia()) return false;
+
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+  const digito2 = this.criaDigito(cpfParcial + digito1);
+
+  const novoCPF = `${cpfParcial}${digito1}${digito2}` 
+  
+  return novoCPF === this.cpfLimpo;
+}
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
+  let regressivo = cpfArray.length + 1;
+
+  const total = cpfArray.reduce((acumulador, valor) => {
+    acumulador += (Number(valor) * regressivo);
+    regressivo--;
+    return acumulador;
+  }, 0);
+
+  const digito = 11 - (total % 11);
+  return digito > 9 ? '0' : String(digito)
+}
+
+ValidaCPF.prototype.isSequencia = function() {
+  return this.cpfLimpo[0].repeat(this.cpfLimpo.length) === this.cpfLimpo; 
+}
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const cpfValido = new ValidaCPF(cpf.value);
+  console.log(cpfValido);
+  console.log(cpfValido.valida());
+});
+
