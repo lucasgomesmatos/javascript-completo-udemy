dd45434380501ca121cdc45a4b6a54cd12dc1cc8
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-01-23 10:02:27 -0300

    Promise

---
+++ assincrono/promises/index.js
@@ -0,0 +1,30 @@
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    if(typeof msg !== 'string') reject('Bad Value');
+    setTimeout(() => {
+      resolve(msg);
+    }, tempo);
+  });
+}
+
+esperaAi("frase 1", rand(1, 3))
+  .then((resposta) => {
+    console.log(resposta);
+    return esperaAi(111, rand(1, 3));
+  })
+  .then((resposta) => {
+    console.log(resposta);
+    return esperaAi("frase 3", rand(1, 3));
+  })
+  .then((resposta) => {
+    console.log(resposta);
+  })
+  .catch(e => {
+    console.log(e)
+  })
