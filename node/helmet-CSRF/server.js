3be8ad28b7db45a9a13ff89df3b59b314b57ae56
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-02-17 23:39:23 -0300

    csrf token no middleware

---
+++ node/helmet-CSRF/server.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+// express
+const express = require("express");
+const app = express();
+
+// mongoose (mongo DB)
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit("pronto");
+  })
+  .catch(e => console.log(e));
+
+// Session require
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+// rotas
+const routes = require("./routes");
+const path = require("path");
+const helmet = require('helmet');
+const csrf = require('csurf')
+const { middlewareGlobal, checkCsrfError, csrfMiddleware} = require("./src/middlewares/middleware");
+
+app.use(helmet());
+
+// requisição do body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public")));
+
+// Session
+const sessionOptions = session({
+  secret: 'dsd[sadsjkbfisbsidfdsikbfsikfsdbfisd()',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+})
+
+app.use(sessionOptions);
+app.use(flash());
+// requisição de views - render
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.use(csrf());
+
+// Middleware
+app.use(middlewareGlobal, checkCsrfError, csrfMiddleware);
+app.use(routes);
+
+app.on("pronto", () => {
+  // servidor express
+  app.listen(3000, () => {
+    console.log("Servidor executando!");
+  });
+});
---
+++ node/helmet-CSRF/src/middlewares/middleware.js
@@ -0,0 +1,16 @@
+exports.middlewareGlobal = (req, res, next) => {
+  res.locals.umaVariavelLocal = "Este é uma valor da variável local.";
+
+  next();
+};
+
+exports.checkCsrfError = (err, req, res, next) => {
+  if(err && 'EBADCSRFTOKEN' === err.code) {
+    return res.render('404');
+  }
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.crsf = req.csrfToken();
+  next();
+}
