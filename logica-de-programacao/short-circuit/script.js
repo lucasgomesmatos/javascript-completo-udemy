aa99d20b2ac45a999658db7e2e7490a1be8fc695
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-08-10 11:09:37 -0300

    short circuit

--- logica-de-programacao/short-circuit/script.js
+++ logica-de-programacao/short-circuit/script.js
@@ -1,1 +1,20 @@
-console.log('Lucas' && true && 'Maria')
+/**
+ * && -> false && true -> false "valor mesmo"
+ *
+ * FALSY
+ * (false, 0, '', "", ``, null, undefined, NaN)
+ *
+ */
+
+// console.log('Lucas' && undefined);
+
+function falaOi() {
+  return 'Oi';
+}
+
+const vaiExecutar = true;
+
+console.log(vaiExecutar && falaOi());
+
+// retorna o primeiro valor verdadeiro
+console.log(0 || false || null || 'Lucas' || true);
