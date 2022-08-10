4b3d83e72d4b7859ebf14716352a6b64006513b3
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-08-10 10:54:19 -0300

    operadores logicos

--- logica-de-programacao/operadores-logicos/script.js
+++ logica-de-programacao/operadores-logicos/script.js
@@ -1,13 +1,20 @@
 /**
  * Operadores Lógicos
  * && -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
- * || -> OR -> OU
+ * || -> OR -> OU ->
  * ! -> NOT -> NÃO
  */
 
 const expressaoAND = true && true && true;
 const expressaoOR = false || true || false;
 
-console.log(true || false);
-console.log(expressaoOR);
-console.log(expressaoAND);
+// console.log(true || false);
+// console.log(expressaoOR);
+// console.log(expressaoAND);
+// console.log(true);
+
+const usuario = 'Lucas';
+const senha = '1234';
+
+const login = usuario === 'Lucas' && senha === '1234';
+console.log(login);
