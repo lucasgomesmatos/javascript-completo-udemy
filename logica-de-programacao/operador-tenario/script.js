c8f06de3d045fba5b082139ae248bed93968d732
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-09-08 08:20:07 -0300

    refatory typescript

--- logica-de-programacao/operador-tenario/script.js
+++ logica-de-programacao/operador-tenario/script.js
@@ -2,9 +2,6 @@
  * Operador Ternário
  * ? :
  */
-
-const pontuacaoUsuario = 999;
-
-const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário normal';
-
+var pontuacaoUsuario = 1999;
+var nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário normal';
 console.log(nivelUsuario);
---
+++ logica-de-programacao/operador-tenario/script.ts
@@ -0,0 +1,10 @@
+/**
+ * Operador Ternário
+ * ? :
+ */
+
+const pontuacaoUsuario: number = 1999;
+
+const nivelUsuario: string = pontuacaoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário normal';
+
+console.log(nivelUsuario);
